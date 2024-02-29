402fdb931318c9034ae12d179b6c8dd0721c2b6a
Jacob Brown <jacobdbrown4@gmail.com>
2024-02-29 16:44:15 -0700

    made register screen scrollable so it works on smaller screens

--- frontend/TodoFromScratch/app/src/main/java/com/example/todofromscratch/RegisterScreen.kt
+++ frontend/TodoFromScratch/app/src/main/java/com/example/todofromscratch/RegisterScreen.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
@@ -89,7 +91,8 @@ fun RegisterScreen(
     Surface () {
         Column(
             modifier = Modifier
-                .fillMaxSize(),
+                .fillMaxSize()
+                .verticalScroll(rememberScrollState()),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
             Spacer(
